c4809aed615697c70abc236398bf1ca52a9e6eb6
Olalekan Rasaq <69814366+Olalekanrasaq@users.noreply.github.com>
2024-07-20 09:52:16 +0100

    Delete static/scripts.js

--- static/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.querySelector('.sidebar-toggle');
-    const sidebar = document.querySelector('.sidebar');
-
-    toggleButton.addEventListener('click', function() {
-        sidebar.classList.toggle('collapsed');
-    });
-});
