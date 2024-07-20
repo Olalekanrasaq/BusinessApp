a082012715a4cf1984758700a352e3d527212850
Olalekan Rasaq <69814366+Olalekanrasaq@users.noreply.github.com>
2024-07-20 09:34:49 +0100

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,13 +1,8 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-    const sidebarToggleButtons = document.querySelectorAll('.sidebar-toggle');
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.querySelector('.sidebar-toggle');
     const sidebar = document.querySelector('.sidebar');
-    const mainContent = document.querySelector('.main-content');
-    
-    sidebarToggleButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            sidebar.classList.toggle('sidebar-closed');
-            mainContent.classList.toggle('full-width');
-        });
+
+    toggleButton.addEventListener('click', function() {
+        sidebar.classList.toggle('collapsed');
     });
 });
